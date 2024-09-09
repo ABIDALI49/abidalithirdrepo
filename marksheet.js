f6c6fa9f38fe9e355b738426c2855e17ece979ec
ABIDALI49 <abidaliqureshi6624@gmail.com>
2024-09-09 18:51:19 +0500

    Add files via upload

---
+++ marksheet.js
@@ -0,0 +1,14 @@
+let marks = prompt("enter your marks")
+if(marks > 80){
+    alert("A1 grade")
+}
+else if (marks > 70){
+    alert("A grade")
+}
+
+else if (marks > 60 ){
+    alert("B grade")
+}
+else{
+    alert("fail")
+}
